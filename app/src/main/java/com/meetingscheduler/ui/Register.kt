b8edf7607ecc2d3c4917d6cbb4c1649b9f9b445a
Maroua <151386570+Maroua512@users.noreply.github.com>
2024-09-19 18:34:26 +0200

    update2

--- app/src/main/java/com/meetingscheduler/ui/Register.kt
+++ app/src/main/java/com/meetingscheduler/ui/Register.kt
@@ -1,3 +1,4 @@
+package com.meetingscheduler.ui
 import com.meetingscheduler.ViewModels.RegisterViewModel
 import android.os.Bundle
 import android.view.View
@@ -5,6 +6,12 @@ import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import com.google.android.material.textfield.TextInputLayout
+
+import com.meetingscheduler.Utils.ConfirmPasswordValidator
+import com.meetingscheduler.Utils.EmailValidation
+import com.meetingscheduler.Utils.PasswordValidation
+import com.meetingscheduler.ValidateService
 
 import com.meetingscheduler.databinding.ActivityRegisterBinding
 
@@ -19,31 +26,33 @@ class Register : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnSignUp.setOnClickListener {
-            val email = binding.email.editText?.text.toString()
-            val password = binding.password.editText?.text.toString()
-            val confirmPassword = binding.ConfirmPassword.editText?.text.toString()
+            val email = binding.email
+            val password = binding.password
+            val confirmPassword = binding.ConfirmPassword
 
             // Validation des champs
             if (validateFields(email, password, confirmPassword)) {
-                viewModel.checkIfUserExists(email)
+                viewModel.checkIfUserExists(email.editText?.text.toString())
             }
         }
 
         observeViewModel()
     }
 
-    private fun validateFields(email: String, password: String, confirmPassword: String): Boolean {
+    private fun validateFields(email: TextInputLayout, password: TextInputLayout, confirmPassword: TextInputLayout): Boolean {
         // Ajouter la logique de validation ici (similaire à l'ancien code)
-        return true
+        val validation = ValidateService(listOf(email to EmailValidation() ,password to PasswordValidation(binding.formatPassword) ,confirmPassword to ConfirmPasswordValidator(password)))
+        return validation.validete()
+
     }
 
     private fun observeViewModel() {
         // Observer les données du ViewModel
-        viewModel.isLoading.observe(this, Observer { isLoading ->
+        viewModel.isLoading.observe(this,  { isLoading ->
             binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
         })
 
-        viewModel.userExists.observe(this, Observer { exists ->
+        viewModel.userExists.observe(this,Observer { exists ->
             if (exists) {
                 val email = binding.email.editText?.text.toString()
                 val password = binding.password.editText?.text.toString()
